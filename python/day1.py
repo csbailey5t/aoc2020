98261a11a865985e902af0fa7be6678cc64231f1
Scott Bailey <csb5t@virginia.edu>
2020-12-01 14:26:53 -0500

    Add python day 1 solution

---
+++ python/day1.py
@@ -0,0 +1,42 @@
+from itertools import combinations
+
+# Part 1
+
+
+def get_product(data):
+    combos = combinations(data, 2)
+    for (x, y) in combos:
+        if (x + y) == 2020:
+            print(x * y)
+            exit
+
+
+def read_file_to_list(fn):
+    with open(fn, "r") as f:
+        data = set([int(ln.strip()) for ln in f.readlines() if ln != ""])
+    return data
+
+
+def partone():
+    data = read_file_to_list("day1-data.txt")
+    get_product(data)
+
+
+# Part 2
+#
+def get_product3(data):
+    combos = combinations(data, 3)
+    for (x, y, z) in combos:
+        if (x + y + z) == 2020:
+            print(x * y * z)
+            exit
+
+
+def parttwo():
+    data = read_file_to_list("day1-data.txt")
+    get_product3(data)
+
+
+if __name__ == "__main__":
+    # partone()
+    parttwo()

2ab3157a2c65fa38e7ecbbb3733680201d8d8191
Scott Bailey <csb5t@virginia.edu>
2020-12-01 16:19:12 -0500

    Add generally working js day1, part 2

--- ts/day1.ts
+++ ts/day1.ts
@@ -5,11 +5,18 @@ const nums: Array<number> = data.split("\n").map(Number)
 // part 1
 for (const num of nums) {
     if (nums.includes(2020 - num) == true) {
-        console.log(num * (2020 - num)) 
-        break; 
+        console.log("part1", num * (2020 - num)) 
+        break 
     }
 }
 
 //part 2
-
+for (const num1 of nums) {
+    for (const num2 of nums) {
+        if (nums.includes(2020 - num1 - num2) == true) {
+            console.log("part2", num1 * num2 * (2020 - num1 - num2))
+            break
+        }
+    }
+}
 

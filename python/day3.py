2180151a7b186b56ff253a3b9ced653d6802dbfa
Scott Bailey <csb5t@virginia.edu>
2020-12-03 16:27:15 -0500

    Add day3, part 2 in python

--- python/day3.py
+++ python/day3.py
@@ -6,7 +6,11 @@
 # - data should be list of list of chars
 
 
-def read_data(fn, dx, dy):
+from functools import reduce
+from operator import mul
+
+
+def read_data(fn):
     with open(fn, "r") as f:
         # Below we repeat the string to account for right movement
         # This is a hack
@@ -27,10 +31,18 @@ def solve(data, x, y, dx, dy, trees):
 
 
 def partone():
-    data = read_data("../data/day3.txt", 3, 1)
+    data = read_data("../data/day3.txt")
     answer = solve(data, 0, 0, 3, 1, 0)
     print(f"there are {answer} trees")
 
 
+def parttwo():
+    data = read_data("../data/day3.txt")
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    answers = [solve(data, 0, 0, dx, dy, 0) for (dx, dy) in slopes]
+    product = reduce(mul, answers, 1)
+    print(product)
+
+
 if __name__ == "__main__":
-    partone()
+    parttwo()

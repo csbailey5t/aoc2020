c1f24ed6cc39b6e00d9849b52d3a6388c6150d09
Scott Bailey <csb5t@virginia.edu>
2020-12-08 16:42:26 -0500

    Add day5, part 2 in python

--- python/day5.py
+++ python/day5.py
@@ -2,6 +2,7 @@ def get_id(row, column):
     return (row * 8) + column
 
 
+# Should abstract the search out of these get_* functions
 def get_row(bpass):
     left = 0
     right = 127
@@ -33,6 +34,16 @@ def partone(fn):
     return max(ids)
 
 
+def parttwo(fn):
+    with open(fn, "r") as f:
+        passes = [line.strip() for line in f]
+    sorted_ids = sorted([get_id(get_row(x), get_col(x)) for x in passes])
+    for i in range(len(sorted_ids) - 1):
+        if sorted_ids[i] + 2 == sorted_ids[i + 1]:
+            return sorted_ids[i] + 1
+
+
 if __name__ == "__main__":
-    answer = partone("../data/day5.txt")
+    # answer = partone("../data/day5.txt")
+    answer = parttwo("../data/day5.txt")
     print(answer)
